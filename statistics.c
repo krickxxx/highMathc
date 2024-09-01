f0f0092028520cf6d3c3a5da051c75dc9b47d8af
krickxxx <45682723+krickxxx@users.noreply.github.com>
2024-09-02 00:00:00 +0500

    'add file statistics.c'

---
+++ statistics.c
@@ -0,0 +1,9 @@
+#include "statistics.h"
+
+double mean(double data[], int size) {
+    double sum = 0.0;
+    for (int i = 0; i < size; i++) {
+        sum += data[i];
+    }
+    return sum / size;
+}
